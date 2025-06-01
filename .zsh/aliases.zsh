a948285f96b96f38ea899753733c82786644dce7
Jared Bydeley <jared@bydeley.dev>
2025-06-01 00:13:43 +0000

    git config stuff

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -7,3 +7,17 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias z="cd"
 
+alias gd="git diff --output-indicator-new=' ' --output-indicator-old=' '"
+alias gs="git status --short"
+
+alias ga="git add --patch"
+alias gc="git commit"
+
+alias gp="git push"
+alias gu="git pull"
+
+alias gl="git log"
+alias gco="git checkout"
+
+alias gi="git init"
+alias gcl="git clone"
